d2976a8d6f63c6e8fa76b1e22d2cf169286954fd
syntacticx <rmm54@njit.edu>
2016-03-20 15:41:51 -0400

    Test of table

--- classes/app.class.php
+++ classes/app.class.php
@@ -13,7 +13,6 @@
      $request_method = $_SERVER['REQUEST_METHOD'];
      $route->$request_method();
      $page_output = $route->getHTML();
-     $page_output .= '<h1>Luna Bear is the best</h1>';
      echo $page_output;
     }
   }
--- classes/homepageController.class.php
+++ classes/homepageController.class.php
@@ -5,7 +5,7 @@
     public function get() {
       $this->html .= '<a href="index.php?controller=carController">CarController</a>';
       session_start();
-      print_r($_SESSION);
+      $this->html .= $_SESSION;
     }
 
     public function post() {}
@@ -14,5 +14,17 @@
 
     public function delete() {}
 
-
+    public function buildTable($array) {
+      var $table;
+      $this->table = '<table><thead><tr><th>';
+      $this->table .= implode('</th><th>', array_keys($array[0]->get_vals()));
+      $this->table .= '</th></tr></thead><tbody>';
+      foreach ($array as $element){
+        $this->table .= '<tr><td>';
+        $this->table .= implode('</td><td>', $element->get_vals());
+        $this->table .= '</td></tr>';
+      }
+      return $table;
     }
+
+  }
